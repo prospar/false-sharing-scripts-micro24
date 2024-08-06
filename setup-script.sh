01bb8cd1f5e8a4e719fcff2c6c55ec6d05fd4b43
vipin <patelvipin11@gmail.com>
2024-08-06 09:38:24 +0530

    fix setup script

--- setup-script.sh
+++ setup-script.sh
@@ -6,7 +6,12 @@ cd /home/prospar/prospar-micro-output
 bash scripts/dir-creation-script.sh
 
 echo "Building the protocols"
+cd /home/prospar/false-sharing-scripts-micro24
 bash build-protocol-script.sh
 
 echo "Extracting the image files"
+cd /home/prospar/false-sharing-micro24/false-sharing-resources/disk-images
 bash extract-image-file.sh
+
+cd /home/prospar/false-sharing-scripts-micro24
+echo "Done Setting up the gem5 environment"
