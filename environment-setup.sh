e481ddfd55b6c6281959fdfc92a12fa6defb927a
vipin <patelvipin11@gmail.com>
2024-08-06 17:19:07 +0530

    script for environment variable

---
+++ environment-setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export MICRO_VIR_ENV=/home/prospar/micro-virtualenv
+export MICRO_OUT=/home/prospar/prospar-micro-output
+export MICRO_RES=/home/prospar/prospar-micro-result
+export HOME_DIR=/home/prospar/micro-virtualenv/false-sharing-micro24
