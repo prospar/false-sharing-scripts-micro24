e3c1351bcc9cb89e7af9a100062ec90aed18da7d
vipin <patelvipin11@gmail.com>
2024-08-07 23:03:10 +0530

    script fix

--- fc-ic-result.sh
+++ fc-ic-result.sh
@@ -2,16 +2,16 @@
 
 echo "Script for fc-ic result"
 
-if [ -d "${MICRO_OUT}/micro-fc-ic-32" && -d "${MICRO_OUT}/micro-fc-ic-32/FS_MESI" ] ; then
+if [ -d "${MICRO_OUT}/micro-fc-ic-32" && -d "${MICRO_OUT}/micro-fc-ic-32/FS_MESI" ]; then
     echo "Directories for fc-ic 32 expeirments exist"
 else
     echo "Run the fc-ic-32 experiments"
     cp ${MICRO_SCRIPT}/config-script/config.ini.fc-ic-32 ${MICRO_SCRIPT}/config.ini
-    bash false-sharing-fc-ic-script.sh false-sharing-app  repair-detect-protocol 32 $1
+    bash false-sharing-fc-ic-script.sh false-sharing-app  repair-detect-protocol 32 ${1}
 fi
 
 # && -d "${MICRO_OUT}/micro-fc-ic-64"
-if [ -d "${MICRO_OUT}/micro-fc-ic-64" && -d "${MICRO_OUT}/micro-fc-ic-64/FS_MESI" ] ; then
+if [ -d "${MICRO_OUT}/micro-fc-ic-64" && -d "${MICRO_OUT}/micro-fc-ic-64/FS_MESI" ]; then
     echo "Directories for fc-ic 64 experiments exist"
 else
     echo "Running the fc-ic-64 experiments"
@@ -19,7 +19,7 @@ else
     bash false-sharing-fc-ic-script.sh false-sharing-app  repair-detect-protocol 64 $1
 fi
 
-if [ -d "${MICRO_OUT}/micro-false-sharing-app" && -d "${MICRO_OUT}/micro-micro-false-sharing-app/FS_MESI"] ; then
+if [ -d "${MICRO_OUT}/micro-false-sharing-app" && -d "${MICRO_OUT}/micro-micro-false-sharing-app/FS_MESI"]; then
     echo "Directories for FS MESI with granularity 1 exist"
 else
     echo "Running the fslite experiments"
