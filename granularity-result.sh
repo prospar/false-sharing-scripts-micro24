9d64717905397b20393252844ee8c85ed3689c16
vipin <patelvipin11@gmail.com>
2024-08-07 22:54:55 +0530

    updated script path

--- granularity-result.sh
+++ granularity-result.sh
@@ -2,21 +2,21 @@
 
 echo "Script for granularity result"
 
-cp ${MICRO_RESOURCE}/scripts/config.ini.tw-2 config.ini
 # && -d "${MICRO_OUT}/micro-granularity-2/FS_MESI_DETECTION"
 if [ -d "${MICRO_OUT}/micro-granularity-2" && -d "${MICRO_OUT}/micro-granularity-32/FS_MESI" ] ; then
     echo "Directories for granularity 2 expeirments exist"
 else
     echo "Running granualrity-2 experiments"
+    cp ${MICRO_SCRIPT}/scripts/config.ini.tw-2 ${MICRO_SCRIPT}/config.ini
     bash false-sharing-granularity-script.sh fslite false-sharing-app 2 $1
 fi
 
-cp ${MICRO_RESOURCE}/scripts/config.ini.tw-4 config.ini
 # && -d "${MICRO_OUT}/micro-granularity-2/FS_MESI_DETECTION"
 if [ -d "${MICRO_OUT}/micro-granularity-4" && -d "${MICRO_OUT}/micro-granularity-4/FS_MESI" ] ; then
     echo "Directories for granularity 4 expeirments exist"
 else
     echo "Run the granularity-4 experiment"
+    cp ${MICRO_SCRIPT}/scripts/config.ini.tw-4 ${MICRO_SCRIPT}/config.ini
     bash false-sharing-granularity-script.sh fslite false-sharing-app 4 $1
 fi
 
@@ -35,11 +35,11 @@ if [ -d ${MICRO_OUT}/micro-false-sharing-app/FS_MESI ]; then
     echo "False Sharing app result with granularity 1 exists"
 else
     echo "running FS MESI with granularity 1"
-    cp ${MICRO_RESOURCE}/scripts/config.ini.32KB config.ini
+    cp ${MICRO_SCRIPT}/scripts/config.ini.32KB ${MICRO_SCRIPT}/config.ini
     bash false-sharing-fslite-script.sh fslite false-sharing-app $1
 fi
 
-cp ${MICRO_RESOURCE}/scripts/config.ini.tw-2 config.ini
+cp ${MICRO_SCRIPT}/scripts/config.ini.tw-4 ${MICRO_SCRIPT}/config.ini
 cp -r ${MICRO_OUT}/micro-false-sharing-app/FS_MESI ${MICRO_OUT}/micro-granularity-output/FS_MESI
 
 # generate the intermediate results for plots
